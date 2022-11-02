12df51e89ee1087de659e89eda7e4545627efe83
mumtazalwan <mumtazalwan@gmail.com>
2022-11-02 16:36:05 +0700

    hold on

---
+++ lib/ui/bottom_navigation/item/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeDashboard extends StatefulWidget {
+  const HomeDashboard({Key? key}) : super(key: key);
+
+  @override
+  State<HomeDashboard> createState() => _HomeDashboardState();
+}
+
+class _HomeDashboardState extends State<HomeDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: SingleChildScrollView()),
+    );
+  }
+}
