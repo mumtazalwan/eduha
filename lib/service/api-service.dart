629524871dcdcc0bbf888d722298eecbdb4da594
hibakun <hiba.kudus@gmail.com>
2022-11-03 20:25:29 +0700

    feat: [HF] detail course

--- lib/service/api-service.dart
+++ lib/service/api-service.dart
@@ -1,4 +1,5 @@
 import 'package:eduha/model/course_learning.dart';
+import 'package:eduha/model/detail_course.dart';
 import 'package:eduha/model/learning_path.dart';
 import 'package:http/http.dart' as http;
 
@@ -31,6 +32,23 @@ class ApiService {
 
       if (response.statusCode == 200) {
         return courseLearningModelFromJson(response.body);
+      } else {
+        throw 'Failed to fetch data from API';
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future getDetailCourse(int id) async {
+    String endPoint = 'learning-path/foundation/material?submaterialId=$id';
+    final url = '$baseUrl$endPoint';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        return detailCourseModelFromJson(response.body);
       } else {
         throw 'Failed to fetch data from API';
       }
